6a09adbd2780090aee8675b433cce42422552c4a
Larissa Paiva <larissapaivadossantos11@gmail.com>
2026-03-10 10:11:40 -0300

    codigo atualizado

--- Lista 1 - Aritmética e Condicionais/questao3.c
+++ Lista 1 - Aritmética e Condicionais/questao3.c
@@ -1,168 +1,108 @@
 #include <stdio.h>
 
-int main(){
-    //definiçao de variaveis 
-    int x, y, z, total;
-    int valorx, valory, valorz;
-    int investimento = 0;
-    int resto;   
-    
+int main() {
+
+    int total, x, y, z;
     scanf("%d", &total);
     scanf("%d", &x);
     scanf("%d", &y);
     scanf("%d", &z);
 
-    // para divisao inteira 
-    if ((total * x) % 100 == 0 && (total * y) % 100 == 0 && (total * z) % 100 == 0){
-        valorx = (total * x) / 100;
-        valory = (total * y) / 100;
-        valorz = (total * z) / 100;
-        
-        resto = total - (valorx + valory + valorz);
-        
+    int A, B, C;
+    int resto;
+    int investiu = 0;
+    int conseguiu_dividir = 0;
+
+    /* verifica se a divis�o original gera valores inteiros */
+    if ((total * x) % 100 == 0 && (total * y) % 100 == 0 && (total * z) % 100 == 0) {
+
+        A = (total * x) / 100;
+        B = (total * y) / 100;
+        C = (total * z) / 100;
+
+        resto = total - (A + B + C);
+
+        conseguiu_dividir = 1;
+
     } else {
-        //para disao com resto (totsal com resto)
-        int tcr = total + 1;
 
-        if ((tcr * x) % 100 == 0 && (tcr * y) % 100 == 0 && (tcr * z) % 100 == 0){
-            valorx = (tcr * x) / 100;
-            valory = (tcr * y) / 100;
-            valorz = (tcr * z) / 100;
-            
-            resto = tcr - (valorx + valory + valorz);
-            
-            if (resto >= 2){ // precisa sobrar mais do que ela investiu
-                investimento = 1;
-            }
-        }
+        /* Rebeka tenta adicionar at� 3 reais */
+        int i = 1;
 
-        // 2 reais investidos 
-        if (investimento == 0) {
-            tcr = total + 2;
+        /* continua enquanto ainda n�o encontrou divis�o v�lida */
+        while (i <= 3 && conseguiu_dividir == 0) {
 
-            if ((tcr * x) % 100 == 0 && (tcr * y) % 100 == 0 && (tcr * z) % 100 == 0){
-                valorx = (tcr * x) / 100;
-                valory = (tcr * y) / 100;
-                valorz = (tcr * z) / 100;
-                
-                resto = tcr - (valorx + valory + valorz);
-                
-                if (resto >= 3){ // precisa sobrar mais do que ela investiu
-                    investimento = 2;
+            int novo_total = total + i;
+
+            if ((novo_total * x) % 100 == 0 &&
+                (novo_total * y) % 100 == 0 &&
+                (novo_total * z) % 100 == 0) {
+
+                int a = (novo_total * x) / 100;
+                int b = (novo_total * y) / 100;
+                int c = (novo_total * z) / 100;
+
+                int r = novo_total - (a + b + c);
+
+                /* precisa sobrar mais do que ela investiu */
+                if (r > i) {
+                    A = a;
+                    B = b;
+                    C = c;
+                    resto = r;
+                    investiu = i;
+                    conseguiu_dividir = 1;
                 }
             }
-        } 
 
-        // 3 reais 
-        if (investimento == 0) {
-            tcr = total + 3;
-
-            if ((tcr * x) % 100 == 0 && (tcr * y) % 100 == 0 && (tcr * z) % 100 == 0){
-                valorx = (tcr * x) / 100;
-                valory = (tcr * y) / 100;
-                valorz = (tcr * z) / 100;
-                
-                resto = tcr - (valorx + valory + valorz);
-                
-                if (resto >= 4){ // precisa sobrar mais do que ela investiu
-                    investimento = 3;
-                }
-            }
-        }  
-        
-        // se der tudo errado 
-        if (investimento == 0) {
-            printf("Nao foi dessa vez que Rebeka pode ajudar...\n");
-            printf("E parece que Rebeka vai ter que voltar andando...\n");
-            return 0;   // parar o programa
+            i++;
         }
     }
 
-    // divisao resultado 
-    printf("Cada homem ficou com %d, %d e %d reais, respectivamente\n",valorx, valory, valorz);
+    int dinheiro_rebeka = 3; /* dinheiro inicial */
 
-    // condicional caso Rebeka tenha investido 2 reais
-    if (investimento == 2) {    
-        char l1, l2, l3;
+    if (conseguiu_dividir) {
 
-        scanf(" %c %c %c", &l1, &l2, &l3);
+        printf("Cada homem ficou com %d, %d e %d reais, respectivamente\n", A, B, C);
 
-        //conversao leras - numeros 
-        int v1 = l1 - 'a' + 1;
-        int v2 = l2 - 'a' + 1;
-        int v3 = l3 - 'a' + 1;
+        dinheiro_rebeka = dinheiro_rebeka - investiu + resto;
 
-        int soma = v1 + v2 + v3;
+        /* desafios dependendo do investimento */
+        if (investiu == 2) {
 
-        printf("%d\n", soma);
+            char a, b, c;
+            scanf(" %c %c %c", &a, &b, &c);
+
+            /* converte letras para valores (a=1, b=2, ..., z=26) */
+            int soma = (a - 'a' + 1) + (b - 'a' + 1) + (c - 'a' + 1);
+
+            printf("%d\n", soma);
+
+        } else if (investiu == 3) {
+
+            int i1, i2, i3;
+            scanf("%d %d %d", &i1, &i2, &i3);
+
+            int parcelas = 0;
+
+            /* soma quantas parcelas do n�mero 3 existem nas idades */
+            if (i1 % 3 == 0) parcelas += i1 / 3;
+            if (i2 % 3 == 0) parcelas += i2 / 3;
+            if (i3 % 3 == 0) parcelas += i3 / 3;
+
+            printf("%d\n", parcelas);
+        }
+
+    } else {
+        printf("Nao foi dessa vez que Rebeka pode ajudar...\n");
     }
 
-    if (investimento == 3){
-        int idade1, idade2, idade3;
-        int cont = 0; // contador 
-        
-        scanf("%d %d %d", &idade1, &idade2, &idade3);
-
-        //fatores de 3 na primeira idade
-        if (idade1 % 3 == 0){
-            cont++;
-            idade1 = idade1 / 3;
-        }
-         if (idade1 % 3 == 0){
-            cont++;
-            idade1 = idade1 / 3;
-        }
-        if (idade1 % 3 == 0){
-            cont++;
-            idade1 = idade1 / 3;
-        }
-        if (idade1 % 3 == 0){
-            cont++;
-        }//fatores de 3 na segunda idade
-        if (idade2 % 3 == 0){
-            cont++;
-            idade2 = idade2 / 3;
-        }
-        if (idade2 % 3 == 0){
-            cont++;
-            idade2 = idade2 / 3;
-        }
-        if (idade2 % 3 == 0){
-            cont++;
-            idade2 = idade2 / 3;
-        }
-        if (idade2 % 3 == 0){
-            cont++;
-        }
-
-        //fatores de 3 na terceira idade
-        if (idade3 % 3 == 0){
-            cont++;
-            idade3 = idade3 / 3;
-        }
-        if (idade3 % 3 == 0){
-            cont++;
-            idade3 = idade3 / 3;
-        }
-        if (idade3 % 3 == 0){
-            cont++;
-            idade3 = idade3 / 3;
-        }
-        if (idade3 % 3 == 0){
-            cont++;
-        }
-
-        printf("%d\n", cont);
-    }
-
-    int dinrebeka = resto;
-
-    if (dinrebeka >= 7){
+    /* verifica se ela conseguiu os 7 reais para o �nibus */
+    if (dinheiro_rebeka >= 7) {
         printf("Ela conseguiu! Rebeka voltou para casa e apanhou da mae por sumir noite passada!\n");
-    }
-    else {
+    } else {
         printf("E parece que Rebeka vai ter que voltar andando...\n");
     }
-    
+
     return 0;
 }
